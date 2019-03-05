a6c3278f2e8cc44b302a568661087147873db121
Rahman Younus <rahman@industrious.agency>
2019-03-05 17:02:39 +0000

    updated transformer

--- src/Kris/LaravelFormBuilder/Generators/Vue.php
+++ src/Kris/LaravelFormBuilder/Generators/Vue.php
@@ -2,24 +2,46 @@
 
 namespace Kris\LaravelFormBuilder\Generators;
 
+use Kris\LaravelFormBuilder\Form;
 use Kris\LaravelFormBuilder\FormBuilder;
 use Kris\LaravelFormBuilder\Transformers;
 
 class Vue
 {
+    /** @var Transformers\Vue  */
     protected $transformer;
+
+    /** @var FormBuilder  */
     protected $builder;
+
+    /** @var Form  */
     protected $form;
+
+    /** @var array  */
     protected $fields;
+
+    /** @var array  */
     protected $model;
+
+    /** @var array  */
     protected $options;
 
+    /**
+     * Vue constructor.
+     */
     public function __construct()
     {
         $this->transformer = resolve(Transformers\Vue::class);
         $this->builder = resolve(FormBuilder::class);
     }
 
+    /**
+     * Set Form Attribute
+     *
+     * @param $class
+     *
+     * @return $this
+     */
     public function setForm($class)
     {
         $this->form = $this->builder->create($class);
@@ -27,6 +49,12 @@ class Vue
         return $this;
     }
 
+    /**
+     * Invoke transformer to generate a schema
+     *
+     * @return $this
+     * @throws \ReflectionException
+     */
     public function transform()
     {
         $this->fields = $this->transformer->transform($this->form);
@@ -34,6 +62,13 @@ class Vue
         return $this;
     }
 
+    /**
+     * Set Model Attribute
+     *
+     * @param array $model
+     *
+     * @return $this
+     */
     public function setModel(array $model = [])
     {
         $this->model = $model;
@@ -41,6 +76,13 @@ class Vue
         return $this;
     }
 
+    /**
+     * Set Options Attribute
+     *
+     * @param array $options
+     *
+     * @return $this
+     */
     public function setOptions(array $options = [])
     {
         $this->options = $options;
@@ -48,6 +90,11 @@ class Vue
         return $this;
     }
 
+    /**
+     * Get raw values
+     *
+     * @return array
+     */
     public function raw()
     {
         return [
@@ -57,6 +104,11 @@ class Vue
         ];
     }
 
+    /**
+     * Get raw values in Json compatible format
+     *
+     * @return string
+     */
     public function toJson()
     {
         return json_encode($this->raw());
--- src/Kris/LaravelFormBuilder/Transformers/Vue.php
+++ src/Kris/LaravelFormBuilder/Transformers/Vue.php
@@ -7,11 +7,19 @@ use Kris\LaravelFormBuilder\Form;
 
 class Vue
 {
+    /**
+     * Transform Form into a VueFormGenerator Schema
+     *
+     * @see https://vue-generators.gitbook.io/vue-generators/
+     *
+     * @param \Kris\LaravelFormBuilder\Form $form
+     *
+     * @return array
+     * @throws \ReflectionException
+     */
     public function transform(Form $form)
     {
         $data = [];
-
-        //        dd($form->getFields());
 
         foreach ($form->getFields() as $field) {
             $data[] = $this->map($field);
@@ -20,67 +28,104 @@ class Vue
         return $data;
     }
 
+    /**
+     * Map Field into correct format
+     *
+     * @param \Kris\LaravelFormBuilder\Fields\FormField $field
+     *
+     * @return array
+     * @throws \ReflectionException
+     */
     protected function map(FormField $field)
     {
         return array_merge([
           'type' => $this->getType($field),
           'inputType' => $field->getType(),
           'label' => $field->getOption('label'),
-          'model' => $field->getRealName(),
         ], $this->additionalMapping($field));
     }
 
+    /**
+     * Help differentiate between different FormFields
+     *
+     * @param \Kris\LaravelFormBuilder\Fields\FormField $field
+     *
+     * @return string
+     * @throws \ReflectionException
+     */
     protected function getType(FormField $field)
     {
         $instance = (new \ReflectionClass($field))->getShortName();
-
         switch ($instance) {
-      case 'InputType':
-        return 'input';
-        break;
+          case 'InputType':
+            return 'input';
+            break;
 
-      case 'SelectType':
-        return 'select';
-        break;
+          case 'SelectType':
+            return 'select';
+            break;
 
-      case 'CheckableType':
-        return $field->getType();
-        break;
+          case 'CheckableType':
+            return $field->getType();
+            break;
 
-      case 'ButtonType':
-        return 'button';
-        break;
+          case 'ButtonType':
+            return 'button';
+            break;
 
-      default:
-        return 'input';
-        break;
+          default:
+            return 'input';
+            break;
+        }
     }
-    }
 
+    /**
+     * Method to add additional attributes to field definition
+     *
+     * @param \Kris\LaravelFormBuilder\Fields\FormField $field
+     *
+     * @return array
+     * @throws \ReflectionException
+     */
     protected function additionalMapping(FormField $field)
     {
         $data = [];
 
-        if ($field->getValue()) {
-            $data['value'] = $field->getValue();
+        if($this->getType($field) !== 'button'){
+            $data['model'] = $field->getRealName();
         }
 
         if ($field->getDefaultValue()) {
             $data['default'] = $field->getDefaultValue();
         }
 
-        //        TODO: Add these to core
-        //        if($field->getRequired()) {
-        //            $data['required'] = $field->getRequired();
-        //        }
-        //
-        //        if($field->getPlaceholder()) {
-        //            $data['placeholder'] = $field->getPlaceholder();
-        //        }
-        //
-        //        if($field->getHint()) {
-        //            $data['hint'] = $field->getHint();
-        //        }
+        if ($field->getOption('choices')) {
+            $data['value'] = $field->getOption('choices');
+        }
+
+        if($field->getOption('required')) {
+            $data['required'] = $field->getOption('required');
+        }
+
+        if($field->getOption('placeholder')) {
+            $data['placeholder'] = $field->getOption('placeholder');
+        }
+
+        if($field->getOption('hint')) {
+            $data['hint'] = $field->getOption('hint');
+        }
+
+        if($field->getOption('min')) {
+            $data['min'] = $field->getOption('min');
+        }
+
+        if($field->getOption('max')) {
+            $data['max'] = $field->getOption('max');
+        }
+
+        if($field->getOption('disabled')) {
+            $data['disabled'] = $field->getOption('disabled');
+        }
 
         return $data;
     }
