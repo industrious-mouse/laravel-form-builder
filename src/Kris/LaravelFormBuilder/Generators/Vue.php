942a3b914feed73203f99fe903ce621a20fc35f7
Rahman Younus <rahman@industrious.agency>
2019-03-05 13:44:32 +0000

    added basic logic

---
+++ src/Kris/LaravelFormBuilder/Generators/Vue.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Kris\LaravelFormBuilder\Generators;
+
+use Kris\LaravelFormBuilder\FormBuilder;
+
+class Vue
+{
+    protected $form;
+    protected $model;
+    protected $options;
+    protected $transformer;
+    protected $builder;
+
+    public function __construct()
+    {
+        $this->transformer = resolve(Transfomers\Vue::class);
+        $this->builder = resolve(FormBuilder::class);
+    }
+
+    public function setForm($class, $method)
+    {
+        $this->form = $this->builder->create($class, $method);
+
+        return $this;
+    }
+
+    public function transform()
+    {
+        $this->fields = $this->transformer->tranform($this->form);
+
+        return $this;
+    }
+
+    public function setModel(array $model = [])
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    public function setOptions(array $options = [])
+    {
+        $this->options = $options;
+
+        return $this;
+    }
+
+    public function raw()
+    {
+        return [
+            'fields' => $this->fields,
+            'model' => $this->model,
+            'options' => $this->options
+        ];
+    }
+
+    public function toJson()
+    {
+        return json_encode($this->raw());
+    }
+}
---
+++ src/Kris/LaravelFormBuilder/Transformers/Vue.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Kris\LaravelFormBuilder\Tranformers;
+
+class Vue
+{
+    public function transform(array $form)
+    {
+        # code...
+    }
+}
